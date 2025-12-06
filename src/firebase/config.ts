960e9481ac88212477c6548bc95f8f316dda0b1f
Siddu Revu <siddurevu001@gmail.com>
2025-12-06 05:45:05 +0000

    Current runner version: '2.329.0' Runner Image Provisioner   Hosted Comp

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:838700804809:web:0fbe05e017987a5acc7b18",
   "apiKey": "AIzaSyDTl5qNi1z2C9LJ6w_wlc4_ksyg9ruwyC8",
   "authDomain": "studio-6931303873-2959a.firebaseapp.com",
+  "storageBucket": "studio-6931303873-2959a.appspot.com",
   "measurementId": "",
   "messagingSenderId": "838700804809"
 };
